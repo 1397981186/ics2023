806ee2aaac245b25da0c88bfc29386c23dd3c5cd
tracer-ics2023 <tracer@njuics.org>
2024-04-07 11:15:32 +0800

    >  compile NEMU 221220000 张三 Linux alemin-ubuntu2204 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:32 up 52 min,  3 users,  load average: 0.55, 0.55, 0.54

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -72,6 +72,16 @@ static int cmd_si(char *args){
 	return 0;
 }	
 
+static int cmd_info(char *args){
+	char *arg = strtok(NULL," ");
+	if(strcmp(arg,"r")==0){
+		isa_reg_display();
+	}
+	return 0 ;
+}
+
+
+
 static struct {
   const char *name;
   const char *description;
@@ -81,6 +91,8 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   { "si", "single exec", cmd_si },
+  { "info -r/w", "show regs/watch", cmd_info },
+
 
   /* TODO: Add more commands */
 
