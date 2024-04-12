9033dca616aa4f5f4d0477dbc629d4f3736dc5f3
tracer-ics2023 <tracer@njuics.org>
2024-04-12 19:28:08 +0800

    >  compile NEMU 221220000 张三 Linux alemin-ubuntu2204 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:28:08 up 29 min,  3 users,  load average: 0.75, 0.92, 0.96

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -237,6 +237,7 @@ Eval_Res eval(int p,int q){
 		printf("p,q is %d %d, op place is %d \n",p,q,op);
 		
 		if(tokens[op].type == TK_POINTER){
+			printf("reading pointer \n");
 			Eval_Res res_pointer_addr = eval(op+1,q);
 			result.res = vaddr_read(res_pointer_addr.res,4);
 			result.ifsuccess = res_pointer_addr.ifsuccess;
