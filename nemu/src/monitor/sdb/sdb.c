25005029baeccbfee296e5276db5177cac747e11
tracer-ics2023 <tracer@njuics.org>
2024-04-12 20:22:59 +0800

    >  compile NEMU 221220000 张三 Linux alemin-ubuntu2204 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:22:59 up  1:23,  3 users,  load average: 0.91, 1.09, 1.24

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -190,7 +190,7 @@ void init_sdb() {
 
 	int expr_res_val = 0;
 	//char * test_exec = "65+(12*3)+99-32*5";
-	char * test_exec = "0x100000";
+	char * test_exec = "*0x100000";
 	bool ifsucess = true;
 	expr_res_val = expr(test_exec,&ifsucess);
 	printf("result exec is %d \n",ifsucess);
