d8b12ffadfd655cd8ebad8e41cd68baded155403
tracer-ics2023 <tracer@njuics.org>
2024-04-24 19:22:01 +0800

    >  compile NEMU 221220000 张三 Linux alemin-ubuntu2204 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:22:01 up 27 min,  3 users,  load average: 0.77, 0.63, 0.55

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,12 +17,6 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-const char *p_regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	printf("doing difftest \n");
