d855b57f5a442771d507f9d388410d4cb279b8da
tracer-ics2023 <tracer@njuics.org>
2024-04-24 19:20:28 +0800

    >  compile NEMU 221220000 张三 Linux alemin-ubuntu2204 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:28 up 25 min,  3 users,  load average: 0.50, 0.59, 0.53

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,9 +17,27 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+const char *p_regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	printf("doing difftest \n");
-  return false;
+	int reg_num = ARRLEN(cpu.gpr);
+	for (int i = 0; i < reg_num; i++) {
+		if (ref_r->gpr[i] != cpu.gpr[i]) {
+			printf("\33[1;31rigster[%d] gpr wrong!right gpr is 0x%x, wrong gpr is 0x%x\n\33[0m", i, ref_r->gpr[i] , cpu.gpr[i]);
+			return false;
+		}
+	}
+		if (ref_r->pc != cpu.pc) {
+	    printf("\33[1;31mpc wrong!right pc is 0x%x, wrong pc is 0x%x\n\33[0m",ref_r->pc, cpu.pc);
+			return false;
+		}
+	return true;
 }
 
 void isa_difftest_attach() {
