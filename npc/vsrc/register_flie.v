6ad672da547ce4da9eff0f031f3a7b522d3eb5e0
tracer-ics2023 <tracer@njuics.org>
2024-05-03 20:01:22 +0800

    >  sim RTL 221220000 张三 Linux alemin-ubuntu2204 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:01:22 up  2:10,  1 user,  load average: 0.59, 0.51, 0.55

--- npc/vsrc/register_flie.v
+++ npc/vsrc/register_flie.v
@@ -14,7 +14,7 @@ module register_file(
 );
 
     integer i;
-    reg[`RegBus] regs[`BitWidth-1 : 0] /* verilator public_flat_rw */;
+    reg[`RegBus] regs[`BitWidth-1 : 0] /* verilator public */;
     wire         r_wen;
 
     assign r_wen = (clk1_flag == 1'b1) & wen;
