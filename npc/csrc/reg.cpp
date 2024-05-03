53329cb4d9e64017c6b4300700a9d6a2e1aa96dc
tracer-ics2023 <tracer@njuics.org>
2024-05-03 19:50:47 +0800

    >  sim RTL 221220000 张三 Linux alemin-ubuntu2204 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:50:47 up  2:00,  1 user,  load average: 0.61, 0.75, 0.62

--- npc/csrc/reg.cpp
+++ npc/csrc/reg.cpp
@@ -8,7 +8,8 @@ extern Vrv32 *top;
 /*********************************************/
 
 
-#define gpr top->rv32__DOT__register_file_inst__DOT__regs
+//#define gpr top->rv32__DOT__register_file_inst__DOT__regs
+#define gpr top->rv32__DOT__register_file__DOT__regs//sht
 
 static const char *regs[] = {
     "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
